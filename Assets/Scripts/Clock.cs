7989a0d95c0caadee40f4dccb8cccaab7fb13928
Wafiki789 <raphaelle.mp@gmail.com>
2023-05-13 17:20:51 -0400

    Exercise 4

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -5,14 +5,26 @@ using System;
 
 public class Clock : MonoBehaviour
 {
-    public GameObject hoursAnchor;
-    public GameObject minutesAnchor;
+    //I would normally use a config file, choose among the needles designed there, and have it explicit in the file
+    //which needle is the hour and which is the minute, so it would be less prone to error than relying on gameobjects order.
+    public List<GameObject> needlesSet;
+
+    GameObject hoursAnchor;
+    GameObject minutesAnchor;
 
     //For tests' sake
     public GameObject secondsAnchor;
 
     //For optimization's sake
     int lastMinute = 0;
+
+    private void Start(){
+        GameObject thisNeedleSet = needlesSet[UnityEngine.Random.Range(0, needlesSet.Count)];
+        hoursAnchor = Instantiate(thisNeedleSet.transform.GetChild(0).gameObject, this.transform.position, Quaternion.identity);
+        hoursAnchor.transform.parent = this.transform;
+        minutesAnchor = Instantiate(thisNeedleSet.transform.GetChild(1).gameObject, this.transform.position, Quaternion.identity);
+        minutesAnchor.transform.parent = this.transform;
+    }
 
     //TODO if time allows: make it so minutes and hours progress slowly? instead of going from one minute to another directly.
     void Update()
